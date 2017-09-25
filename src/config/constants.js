8626995326c6fb1960249b99c7b9e4783293fa33
Carlos Rodrigues <carlosrodriguesjunior@gmail.com>
2017-09-25 15:35:23 -0300

    yml

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-const sendGridKey = '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';
+const sendGridKey = 'CHAVE DO SEND GRID';
 const QUANTITY_PER_PAGE = 10;
 
 module.exports = {
